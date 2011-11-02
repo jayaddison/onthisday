240e0b43a481b93b7cf2583ff14495d027fa5534
James Addison <55152140+jayaddison@users.noreply.github.com>
2011-11-02 16:58:48 +0000

    Data generation utilities

---
+++ utils/extract.py
@@ -0,0 +1,58 @@
+import sys
+import urllib2
+import simplejson as json
+import logging
+import re
+
+from mwlib.parser import Section, Item
+from mwlib.uparser import parseString
+from mwlib.utils import fetch_url
+from mwlib import advtree
+
+logging.basicConfig()
+
+# Check that a date has been specified
+if not len(sys.argv) == 2:
+
+	print 'Please provide exactly one argument - the title page of the date to extract events from in wikipedia (e.g. January_1)'
+	sys.exit(1)
+
+# Gather content from wikipedia
+title = sys.argv[1]
+page = urllib2.urlopen('http://en.wikipedia.org/w/index.php?action=raw&title=%s' % title).read().decode('utf-8')
+tree = parseString(title, page)
+
+# Iterate through the items listed in the wiki page
+results = {}
+for node in tree.allchildren():
+
+	if isinstance(node, Section) and node.children[0].asText().strip() in (u'Events', u'Births', u'Deaths'):
+
+		section = node.children[0].asText().strip().lower().encode('utf-8')
+
+		results[section] = []
+
+		for item in [x.children[0] for x in node.children[1].allchildren() if isinstance(x, Item)]:
+
+			# Add some utility methods to the item
+			advtree.extendClasses(item)
+
+			# Extract the year and text from the item
+			raw = item.getAllDisplayText().strip()
+			matches = re.match(u'(.*)[\s]*(\u2013|&ndash;)[\s]*(.*)', raw)
+
+			if not matches:
+				logging.log(logging.WARN, '%s - Unable to split year and text from string: "%s"' % (title, raw))
+				continue
+
+			year = matches.group(1).strip()
+			text = matches.group(3).strip()
+
+			result = {
+				'year': year,
+				'text': text
+			}
+
+			results[section].append(result)
+
+print json.dumps(results, separators=(',', ':'), ensure_ascii=False).encode('utf-8')
---
+++ utils/gather.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+BASE=`dirname $0`
+
+for month in `seq 1 12`;
+do
+	monthdir=${BASE}/data/${month};
+	monthname=`date -d 2000-${month}-01 +%B`;
+
+	mkdir -p ${monthdir};
+	for day in `cal ${month} 2000 | tail -n +3`;
+	do
+		dayfile=${monthdir}/${day}.json;
+
+		if [ ! -f ${dayfile} ];
+		then
+			python extract.py ${monthname}_${day} > ${dayfile};
+			sleep 0.5;
+		fi;
+	done;
+done;
